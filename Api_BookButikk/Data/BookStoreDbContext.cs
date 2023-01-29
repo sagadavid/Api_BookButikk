bff47124179f627f400eb572ef1438fb0c3aa0fb
sagadavid <epostsaga@gmail.com>
2023-01-29 08:53:29 +0100

    Add project files.

---
+++ Api_BookButikk/Data/BookStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api_BookButikk.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext
+            (DbContextOptions <BookStoreDbContext> contextOptions)
+            :base(contextOptions)
+        {
+        }
+
+        public DbSet<Books> DbSBooks { get; set; }
+
+    }
+}
