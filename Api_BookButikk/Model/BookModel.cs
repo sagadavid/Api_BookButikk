e5af646b8ab2a692cfd49d4d76a306c36c9b6427
sagadavid <epostsaga@gmail.com>
2023-01-30 22:49:20 +0100

    model validation example, required with error message

--- Api_BookButikk/Model/BookModel.cs
+++ Api_BookButikk/Model/BookModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Api_BookButikk.Model
+﻿using System.ComponentModel.DataAnnotations;//to implement model validations
+
+namespace Api_BookButikk.Model
 {
     public class BookModel
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="please add a title")]//validation
         public string Title { get; set; }
         public string Description { get; set; }
     }
