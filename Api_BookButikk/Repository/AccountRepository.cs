1feefa35d6dc8bd550285418fbe7fe865a42b8a5
sagadavid <epostsaga@gmail.com>
2023-02-01 18:53:07 +0100

    account repo loaded with signup meethod, a new user available to create  by signupmodel

--- Api_BookButikk/Repository/AccountRepository.cs
+++ Api_BookButikk/Repository/AccountRepository.cs
@@ -1,11 +1,34 @@
-﻿namespace Api_BookButikk.Repository
+﻿using Api_BookButikk.Model;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Api_BookButikk.Repository
 {
     public class AccountRepository : IAccountRepository 
     {
-        //register repo in seervice configuration/addtransient
-        public AccountRepository()
-        {
+        private readonly UserManager<ApplicationUser> _userManager;
 
+        //register repo in seervice configuration/addtransient
+        //to sign up, use identitycore's usermanager for application user
+        public AccountRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> SignUp(SignUpModel signUpModel)//pass this to iaccountrepo also
+        {
+            //add new user by usermanager
+            var user = new ApplicationUser()
+            {
+                //all properties come from identityuser
+                FirstName = signUpModel.FirstName,
+                LastName = signUpModel.LastName,
+                Email = signUpModel.Email,
+                UserName = signUpModel.Email
+            };
+            //now create this user by password
+            return await _userManager.CreateAsync(user, signUpModel.Password);
+        
         }
     }
 }
--- Api_BookButikk/Repository/IAccountRepository.cs
+++ Api_BookButikk/Repository/IAccountRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Api_BookButikk.Repository
+﻿using Api_BookButikk.Model;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Api_BookButikk.Repository
 {
     public interface IAccountRepository
     {
+        Task<IdentityResult> SignUp(SignUpModel signUpModel)
     }
 }
